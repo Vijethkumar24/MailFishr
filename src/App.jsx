994c932a76004f844b1b7264f52be692d5f5da6f
vkvijeth18 <vkvijethkumar24@gmail.com>
2025-05-21 13:24:13 +0530

    new commit2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import NavBar from "./common/NavBar.jsx";
 import FeatureSection from "./components/FeatureSection.jsx";
 import FooterSection from "./components/FooterSection.jsx";
 import HeroSection from "./components/HeroSection.jsx";
-import HomePage from "./components/HomePage";
 import PartnersSection from "./components/Partners.jsx";
 import ReviewSection from "./components/ReviewSection.jsx";
 import UseCaseSection from "./components/UseCaseSection.jsx";
 
 function App() {
-
-
   return (
     <>
       <NavBar />
